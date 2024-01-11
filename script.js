54e7381da8297fce7782dc2caabe79a78bc127fa
CoderSid063 <sudhansupanda063@gmail.com>
2024-01-11 07:51:10 +0530

    feat: completed development of content-box2

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+var options0 = {
+  series: [
+    {
+      name: "Net Profit",
+      data: [40, 45, 50, 55, 60, 65, 70, 75, 80],
+    },
+  ],
+  chart: {
+    type: "bar",
+    height: 240,
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false,
+      columnWidth: "40%",
+      endingShape: "rounded",
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    show: true,
+    width: 5,
+    colors: ["transparent"],
+  },
+  xaxis: {
+    categories: ["20", "25", "30", "35", "40", "50", "60", "70", "80"],
+  },
+  yaxis: {
+    title: {
+      text: "$ (thousands)",
+    },
+  },
+  fill: {
+    opacity: 1,
+  },
+  tooltip: {
+    y: {
+      formatter: function (val) {
+        return "$ " + val + " thousands";
+      },
+    },
+  },
+};
+
+var chart0 = new ApexCharts(document.querySelector("#chart0"), options0);
+chart0.render();
+
+// RadialChart
+
 var options = {
   series: [78],
   chart: {
